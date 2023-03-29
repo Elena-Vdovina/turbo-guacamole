52d3b8c613ea835ccf05e1250456714adad09650
Elena Sergienko <marketing.seo.web2020@gmail.com>
2023-03-30 02:30:22 +0300

    Add compareTo check

--- src/EventDateNameComparator.java
+++ src/EventDateNameComparator.java
@@ -6,8 +6,11 @@ public class EventDateNameComparator implements Comparator<Event> {
   public int compare(Event o1, Event o2) {
     if (!o1.getDate().equals(o2.getDate())) {
       return o1.getDate().compareTo(o2.getDate());
+    } else if (!o1.getName().equals(o2.getName())) {
+      return o1.getName().compareTo(o2.getName());
+    } else {
+      return o1.getCheck().compareTo(o2.getCheck());
     }
-    return o1.getName().compareTo(o2.getName());
   }
 
 }
