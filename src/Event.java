bce8fa2f6a8094f4cbbad9c6b036965ee7626835
Elena Vdovina <vdovina.ev.1@gmail.com>
2023-03-31 10:20:35 +0200

    Add field priority, getPriorityStr(), getPriority(), setPriority()

--- src/Event.java
+++ src/Event.java
@@ -10,13 +10,19 @@ public class Event {
   public static final String ANSI_YELLOW = "\u001B[33m";
   private static final DateFormat formatter = new SimpleDateFormat("dd.MM.yyyy");
   private final String name;
+  private int priority;
   private Date date;
   private Boolean check;
 
-  public Event(String name, String dateStr, int status) throws ParseException {
+  public Event(String name, int priority, String dateStr, int status) throws ParseException {
     this.name = name;
+    this.priority = priority;
     setDate(dateStr);
     setCheck(status);
+  }
+
+  public void setPriority(int priority) {
+    this.priority = priority;
   }
 
   public void setDate(String dateStr) throws ParseException {
@@ -33,6 +39,21 @@ public class Event {
 
   public String getName() {
     return name;
+  }
+
+  public int getPriority() {
+    return priority;
+  }
+
+  public String getPriorityStr() {
+    String prioritet = "";
+    switch (this.priority) {
+      case 1 -> prioritet = "Очень важно";
+      case 2 -> prioritet = "Важно";
+      case 3 -> prioritet = "Не важно";
+      case 4 -> prioritet = "Неизвестно";
+    }
+    return prioritet;
   }
 
   public Date getDate() {
@@ -55,6 +76,7 @@ public class Event {
     } else {
       checkToPrint = ANSI_YELLOW + "Не выполнено" + ANSI_RESET;
     }
-    return String.format("%-30s %-10s %s", name, formatter.format(date), checkToPrint);
+    String prioritet = getPriorityStr();
+    return String.format("%-30s %-15s %-10s %s", name, prioritet, formatter.format(date), checkToPrint);
   }
 }
